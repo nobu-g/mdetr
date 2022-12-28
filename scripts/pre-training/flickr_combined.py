2ef2eff7d245d0dc8215cb4b12594122994410cb
nobu-g <ueda-n127@hotmail.co.jp>
2022-12-28 20:57:49 +0900

    add flickr_combined.py

--- scripts/pre-training/flickr_combined.py
+++ scripts/pre-training/flickr_combined.py
@@ -10,6 +10,7 @@ https://github.com/BryanPlummer/flickr30k_entities/blob/68b3d6f12d1d710f96233f6b
 import argparse
 import json
 import os
+import re
 from collections import defaultdict
 from pathlib import Path
 from typing import Any, Dict, List, Tuple
@@ -140,6 +141,50 @@ def get_sentence_data(filename) -> List[Dict[str, Any]]:
 
         annotations.append(sentence_data)
 
+    return annotations
+
+
+def get_sentence_data_ja(fn):
+    # exapmle: 5:[/EN#550/clothing 赤い服]を着た4:[/EN#549/people 男]が6:[/EN#551/other 綱]を握って見守っている間に、1:[/EN#547/people 数人のクライマー]が2:[/EN#554/other 列]をなして3:[/EN#548/other 岩]をよじ登っている。
+    tag_pat = re.compile(r'\d+:\[/EN#(?P<id>\d+)(/(?P<type>[a-z]+))+ (?P<words>[^]]+)]')
+    annotations = []
+    for line in Path(fn).read_text().splitlines():
+        chunks = []
+        raw_sentence = ''
+        sidx = 0
+        matches: list[re.Match] = list(re.finditer(tag_pat, line))
+        for match in matches:
+            # chunk 前を追加
+            if sidx < match.start():
+                text = line[sidx:match.start()]
+                raw_sentence += text
+                chunks.append(text)
+            # match の中身を追加
+            raw_sentence += match.group('words')
+            chunks.append({
+                'phrase': match.group('words'),
+                'phrase_id': match.group('id'),
+                'phrase_type': match.group('type'),
+            })
+            sidx = match.end()
+        raw_sentence += line[sidx:]
+        sentence = ''
+        phrases = []
+        char_idx = 0
+        for chunk in chunks:
+            if isinstance(chunk, str):
+                sentence += chunk
+                char_idx += len(chunk)
+            else:
+                chunk['first_char_index'] = char_idx
+                sentence += chunk['phrase']
+                char_idx += len(chunk['phrase'])
+                phrases.append(chunk)
+        assert 'EN' not in sentence
+        annotations.append({
+            'sentence': sentence.strip(' '),
+            'phrases': phrases,
+        })
     return annotations
 
 
@@ -302,7 +347,7 @@ def convert(
 
             target_bboxes = merged_bboxes
 
-        sents = get_sentence_data(flickr_path / "Sentences" / f"{img_id}.txt")
+        sents = get_sentence_data_ja(flickr_path / "Sentences" / f"{img_id}.txt")
         for sent_id, sent in enumerate(sents):
 
             spans = {}  # global phrase ID to span in sentence
@@ -314,10 +359,10 @@ def convert(
             for global_phrase_id, phrase in enumerate(sent["phrases"]):
                 phraseid2entityid[global_phrase_id] = int(phrase["phrase_id"])
                 entityid2phraseid[int(phrase["phrase_id"])].append(global_phrase_id)
-                first_word = phrase["first_word_index"]
-                beg = sum([len(x) for x in sentence.split()[:first_word]]) + first_word
-                spans[global_phrase_id] = (beg, beg + len(phrase["phrase"]))
-                assert sentence[beg : beg + len(phrase["phrase"])] == phrase["phrase"]
+                first_char_index = phrase["first_char_index"]
+                begin, end = first_char_index, first_char_index + len(phrase["phrase"])
+                spans[global_phrase_id] = (begin, end)
+                assert sentence[begin : end] == phrase["phrase"]
 
             all_boxes_in_sent = []
             for ent_id in entity_ids:
@@ -376,7 +421,7 @@ def convert(
         filename = f"final_flickr_separateGT_{subset}.json"
 
     with open(output_path / filename, "w") as j_file:
-        json.dump(ds, j_file)
+        json.dump(ds, j_file, ensure_ascii=False, indent=2)
     return next_img_id, next_id
 
 
