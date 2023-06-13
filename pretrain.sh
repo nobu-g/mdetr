51331a90de3c02afe8bfe22b27309a90d6a43d05
nobu-g <ueda-n127@hotmail.co.jp>
2023-06-13 19:51:17 +0900

    fix pretrain.sh

--- pretrain.sh
+++ pretrain.sh
@@ -17,24 +17,9 @@ poetry run python -m torch.distributed.run --nproc_per_node="${DEVICES}" main.py
   --backbone "${IMAGE_BACKBENE}" \
   --lr_backbone 5e-5 \
   --freeze_text_encoder \
+  --skip_loading_text_encoder \
   --batch_size "${BATCH_SIZE}" \
-  --epochs 1 \
-  --output_dir ./result/pretrained_b3_roberta_ja_mixed_1e \
+  --epochs 2 \
+  --output_dir ./result/pretrained_b3_roberta_ja_mixed_2e \
   --load ./data/official_ckpt/pretrained_EB3_checkpoint.pth \
-  --num_workers 8
-
-# settings on moss110
-# poetry run python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
-# poetry run python main.py \
-poetry run python -m torch.distributed.run --nproc_per_node="${DEVICES}" main.py \
-  --dataset_config configs/pretrain_ja_flickr.json \
-  --ema \
-  --text_encoder_type "${TEXT_ENCODER}" \
-  --backbone "${IMAGE_BACKBENE}" \
-  --lr_backbone 5e-5 \
-  --freeze_text_encoder \
-  --batch_size "${BATCH_SIZE}" \
-  --epochs 1 \
-  --output_dir ./result/pretrained_b3_roberta_ja_mixed_1e_flickr_1e \
-  --load ./result/pretrained_b3_roberta_ja_mixed_1e/checkpoint.pth \
   --num_workers 8
