09c38967b2d19360f5515c81191df736a235d29f
nobu-g <ueda-n127@hotmail.co.jp>
2023-10-19 20:41:30 +0900

    fix scripts

--- fine-tune_mmdialogue_b5.sh
+++ fine-tune_mmdialogue_b5.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 TEXT_ENCODER=xlm-roberta-base  # xlm-roberta-base or microsoft/mdeberta-v3-base
-IMAGE_BACKBENE=timm_tf_efficientnet_b3_ns
-DEVICES=2
-BATCH_SIZE=4
+IMAGE_BACKBENE=timm_tf_efficientnet_b5_ns
+DEVICES=4
+BATCH_SIZE=2
 
 poetry run python -m torch.distributed.run --nproc_per_node="${DEVICES}" main.py \
   --dataset_config configs/mmdialogue.json \
@@ -15,6 +15,6 @@ poetry run python -m torch.distributed.run --nproc_per_node="${DEVICES}" main.py
   --lr_backbone 5e-5 \
   --batch_size "${BATCH_SIZE}" \
   --epochs 4 \
-  --output_dir ./result/pretrained_b3_roberta_ja_mixed_2e_mmdialogue_4e_b8 \
-  --load ./result/pretrained_b3_roberta_ja_mixed_2e/checkpoint.pth \
+  --output_dir ./result/pretrained_b5_roberta_ja_mixed_2e_mmdialogue_4e_b8 \
+  --load ./result/pretrained_b5_roberta_ja_mixed_2e/checkpoint_1.pth \
   --num_workers 8
